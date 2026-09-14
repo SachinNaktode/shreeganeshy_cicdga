d52657598369a578202d5506991cc11cb16c5dde
Sachin Naktode <sachin.naktode.ext@constl.com>
2026-09-14 21:41:02 +0530

    Fix NSG subnet association3

---
+++ .github/workflows/Untitled-1tfsecurity.go
@@ -0,0 +1,13 @@
+plugin "terraform" {
+  enabled = true
+
+  preset = "recommended"
+}
+
+rule "terraform_required_providers" {
+  enabled = false
+}
+
+rule "terraform_required_version" {
+  enabled = false
+}
